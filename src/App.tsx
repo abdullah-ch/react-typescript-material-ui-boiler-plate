0658cf23bf834177b045fac64d42d567e30777ec
abdullah-ch <abdullahchaudharry@gmail.com>
2021-05-23 20:47:18 +0500

    added boiler plate for react-typescript-material-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import theme from './theme/theme';
+import './theme/App.global.css';
+import { ThemeProvider } from '@material-ui/styles';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <ThemeProvider theme={theme}>
+            <h1>React-TypeScript-Boiler-Plate</h1>
+        </ThemeProvider>
+    );
+};
 
 export default App;

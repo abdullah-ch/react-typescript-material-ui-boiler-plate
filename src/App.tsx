747e94cc3f9e7c2bb3d85692d0b0f3807d643101
“abdullah-ch” <abdullahchaudharry@gmail.com>
2021-11-20 23:09:18 +0500

    added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import theme from './theme/theme';
 import './theme/App.global.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from '@material-ui/styles';
-import Home from './pages/Home';
-
-// Recording
-// Each Meeting Tasks
+import Home from './pages/home';
 
 const App: React.FC = () => {
   return (
     <ThemeProvider theme={theme}>
-      <Home />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </ThemeProvider>
   );
 };
